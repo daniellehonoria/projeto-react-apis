082dbb2fe43f05e942c4f24c75440da41e19dcfc
daniellehonoria <daniellehonoria@gmail.com>
2023-02-24 13:01:16 -0300

    renderizou imagens pokemons

--- pokedex-danielle/src/components/Card/Card.js
+++ pokedex-danielle/src/components/Card/Card.js
@@ -2,10 +2,11 @@ import  {CardContainer} from"./CardStyled"
 
 const Card = (props) => {
   const{ pokemon} = props
+  console.log("aqui", pokemon.sprites)
   return (
     <CardContainer>
       <h2>{pokemon.id}</h2>
-      <img src={pokemon.img} alt={pokemon.name}/>
+      <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
       <h2>{pokemon.name}</h2>
       </CardContainer>
   )
